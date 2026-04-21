b4ea0c5d65afba18ca612d488531699708d7ff01
SeniorIntern <tnikhil.official@gmail.com>
2026-04-22 00:18:09 +0400

    Disable file preview

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,14 +1,17 @@
 return {
 	"nvim-telescope/telescope.nvim",
-
 	tag = "0.1.5",
-
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
-
 	config = function()
-		require("telescope").setup({})
+		require("telescope").setup({
+			pickers = {
+				find_files = {
+					previewer = false,
+				},
+			},
+		})
 
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
