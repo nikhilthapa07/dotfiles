9f46eac4d84ecd7ecc71155786e88960d4084d05
SeniorIntern <tnikhil.official@gmail.com>
2026-05-30 16:25:50 +0400

    Change config for neovim-0.11

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,25 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		branch = "main",
-		config = function()
-			require("nvim-treesitter.config").setup({
-				install_dir = vim.fn.stdpath("data") .. "/site",
-			})
-		end,
-	},
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			-- A list of parser names, or "all"
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"javascript",
+				"typescript",
+				"tsx",
+				"html",
+				"css",
+				"vue",
+				"python",
+				"json",
+				"gitignore"
+			},
+		})
+	end,
 }
