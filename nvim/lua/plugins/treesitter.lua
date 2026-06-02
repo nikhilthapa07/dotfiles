3b0460f749ee64b7a8d5d189b185912467de8ab4
SeniorIntern <tnikhil.official@gmail.com>
2026-06-03 00:24:18 +0400

    Fix treesitter for neovim 0.12.*

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,10 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
-	lazy = false,
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- A list of parser names, or "all"
-			ensure_installed = {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		lazy = false,
+		init = function()
+			local parsers = {
 				"lua",
 				"vim",
 				"vimdoc",
@@ -18,8 +16,29 @@ return {
 				"vue",
 				"python",
 				"json",
-				"gitignore"
-			},
-		})
-	end,
+				"gitignore",
+			}
+
+			local group = vim.api.nvim_create_augroup("ThePrimeagenTreesitter", { clear = true })
+			vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
+				group = group,
+				callback = function()
+					if vim.bo.buftype ~= "" then
+						return
+					end
+
+					pcall(vim.treesitter.start, 0)
+				end,
+			})
+
+			vim.api.nvim_create_autocmd("User", {
+				group = group,
+				pattern = "VeryLazy",
+				once = true,
+				callback = function()
+					require("nvim-treesitter").install(parsers)
+				end,
+			})
+		end,
+	},
 }
