a855777cb582503c664e089881630505089666e0
SeniorIntern <tnikhil.official@gmail.com>
2026-06-12 00:21:36 +0400

    Get rid of unpredictable key binds

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -36,10 +36,6 @@ set("n", "<Left>", ":bprevious<CR>")
 -- set("i", "jj", "<ESC>")
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<M-h>", "<cmd>silent !tmux neww tmux-sessionizer -s 0<CR>")
-vim.keymap.set("n", "<M-t>", "<cmd>silent !tmux neww tmux-sessionizer -s 1<CR>")
-vim.keymap.set("n", "<M-n>", "<cmd>silent !tmux neww tmux-sessionizer -s 2<CR>")
-vim.keymap.set("n", "<M-s>", "<cmd>silent !tmux neww tmux-sessionizer -s 3<CR>")
 
 -- save current buffer
 set("n", "<leader>w", "<cmd>noautocmd write<CR>", {
