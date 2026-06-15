eca59854af326861ea42be9e3f2116d52d487ef8
SeniorIntern <tnikhil.official@gmail.com>
2026-06-15 22:10:35 +0400

    Add group for LspAttach auto command

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,10 +1,11 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd("LspAttach", {
-    callback = function(e)
-        vim.keymap.set("n", "grd", vim.lsp.buf.definition, {
-            buffer = e.buf,
-            desc = "LSP: Go to Definition",
-        })
-    end,
+	group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+	callback = function(e)
+		vim.keymap.set("n", "grd", vim.lsp.buf.definition, {
+			buffer = e.buf,
+			desc = "LSP: Go to Definition",
+		})
+	end,
 })
