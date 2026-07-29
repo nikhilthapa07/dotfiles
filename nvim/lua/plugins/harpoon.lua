01c457b579cfddd776b3fe928d1ed7488819f19b
Nikhil Thapa <tnikhil.official@gmail.com>
2026-07-29 22:17:10 +0400

    Fix typo

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -26,7 +26,7 @@ return {
 		vim.keymap.set("n", "<C-k>", function()
 			harpoon:list():select(3)
 		end)
-		vim.keymap.set("n", "<C-k>", function()
+		vim.keymap.set("n", "<C-l>", function()
 			harpoon:list():select(4)
 		end)
 	end,
