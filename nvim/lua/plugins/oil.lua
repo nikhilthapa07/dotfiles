8cdd0cd86026f741832e072cdda4c0b42f15856e
nikhil <nikhil@alternative.agency>
2026-08-26 15:21:07 +0400

    Unbind C-h - Reserved by harpoon

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,7 +2,11 @@ return {
 	"stevearc/oil.nvim",
 	lazy = false,
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+			keymaps = {
+				["<C-h>"] = false,
+			},
+		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Oil: Open parent directory" })
 	end,
 }
